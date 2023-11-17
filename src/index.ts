41d5f83e8298b4f9a49f87665d1b379a38f420a8
louis-marceron <louis.marceron@protonmail.com>
2023-11-17 14:21:48 +0100

    feat: send hello world on get /

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+import express from 'express'
+
+const PORT = 3000
+const app = express()
+
+app.get('/', (_req, res) => {
+   res.send('Hello Word!') 
+})
+
+app.listen(PORT, () => {
+    console.log('localhost:' + PORT)
+})
