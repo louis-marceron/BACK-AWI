8382de04762733fe03f47b8116ba92fdd78636aa
louis-marceron <louis.marceron@protonmail.com>
2023-11-19 10:51:46 +0100

    feat: send 404 error

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import notFoundErrorHandler from './common/notFoundErrorHandler.js'
 
 const app = express()
 
 app.get('/', (_req, res) => {
-   res.send('Hello Word!') 
+   res.send('Hello Word!')
 })
+
+app.use(notFoundErrorHandler)
 
 export default app
